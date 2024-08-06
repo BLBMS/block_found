e41f525f7489f3a35ac5db9f56f54fb5ae96c2e8
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-06 22:07:05 +0200

    Update start_block.sh

--- start_block.sh
+++ start_block.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 # v.2024-08-06
 # by blbMS
-
-# Read coin list from JSON
 coin_list=$(jq -r '.coin_list[]' block_data.json)
 my_github=$(jq -r '.my_github' block_data.json)
-
 FILE="block_update.sh"
 cd ~/
 rm -f $FILE
 wget -q "$my_github$FILE"
 chmod +x $FILE
-
-# Create initial coin files
 echo "$coin_list" | while read -r coin; do
     block_file="block_${coin}.list"
     if [[ ! -f "$block_file" || ! -s "$block_file" ]]; then
@@ -20,7 +15,6 @@ echo "$coin_list" | while read -r coin; do
         echo -e "New file created: \e[1;92m$block_file\e[0m"
     fi
 done
-
 screen -wipe 1>/dev/null 2>&1
 cd ~/
 if screen -list | grep -q "block_update"; then
@@ -32,5 +26,5 @@ else
     screen -wipe 1>/dev/null 2>&1
     screen -dmS block_update 1>/dev/null 2>&1
     screen -S block_update -X stuff "~/block_update.sh\n" 1>/dev/null 2>&1
-    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/Update/\x1b[36m&\x1b[0m/g; s/Watch/\x1b[33m&\x1b[0m/g; s/block_update/\x1b[1;35m&\x1b[0m/g" | tail -n +2 | head -n -1
+    screen -ls | sed -E "s/CCminer/\x1b[32m&\x1b[0m/g; s/block_update/\x1b[1;35m&\x1b[0m/g" | tail -n +2 | head -n -1
 fi
