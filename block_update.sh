2e2247f458c42e2caee66e9eb4afafaf24ee86a0
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-08-12 20:21:11 +0200

    Update block_update.sh

--- block_update.sh
+++ block_update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # v.2024-08-06
-# by blbMS
+
+# Read data from JSON
 my_github=$(jq -r '.my_github' block_data.json)
 coin_list=$(jq -r '.coin_list[]' block_data.json)
 freq=$(jq -r '.freq' block_data.json)
+
+# Printout of the last 5 already saved blocks
 echo "$coin_list" | while read -r coin; do
     echo -e "\e[4m\e[1;93mLast 5 blocks: \e[1;91m$coin\e[0m:\e[24m"
     block_file="block_${coin}.list"
@@ -13,37 +16,57 @@ echo "$coin_list" | while read -r coin; do
         echo -e "\e[90mNo valid block data available.\e[0m"
     fi
 done
+
+# Convert frequency to seconds
 freq_seconds=$(awk "BEGIN {print int($freq * 3600)}")
 if [[ $freq_seconds -lt 3600 ]]; then
     echo -e "\n\e[1;93mCapture frequency: \e[1;91m$freq_seconds\e[0m (sec)\n"
 else
     echo -e "\n\e[1;93mCapture frequency: \e[1;91m$freq\e[0m (h)\n"
 fi
+
+# Function to execute block_found.sh
 execute_block_found() {
     echo -e "\e[96m== $(date +%Y-%m-%d\ %H:%M:%S) \e[0m== ($iter)"
-    FILE="block_found.sh"
     cd ~/
+    FILE="block_sort.py"
+    rm -f $FILE
+    wget -q "$my_github$FILE"
+    FILE="block_found.sh"
     rm -f $FILE
     wget -q "$my_github$FILE"
     chmod +x $FILE
     source ./$FILE
     iter=$((iter + 1))
 }
+
+# Perform the first data capture immediately
 iter=1
 execute_block_found
+
+# Calculate the time of the first capture after midnight that has already passed
 current_time=$(date +%s)
 last_midnight=$(date -d "today 00:00" +%s)
 next_capture_time=$last_midnight
+
+# Add intervals based on the frequency until we reach the current time
 while [[ $next_capture_time -lt $current_time ]]; do
     next_capture_time=$((next_capture_time + freq_seconds))
 done
+
+# Display the time of the next data capture
 echo -e "Next capture time: \e[93m$(date -d @$next_capture_time +%Y-%m-%d\ %H:%M:%S)\e[0m\033[A\033[K\033[A\033[K"
+
 while true; do
     current_time=$(date +%s)
+    # Reset is_found to "no"
     jq '.is_found = "no"' block_data.json > tmp.$$.json && mv tmp.$$.json block_data.json
+    # Check if the current time is equal to or greater than the next capture time
     if [[ "$current_time" -ge "$next_capture_time" ]]; then
         execute_block_found
+        # Set the next data capture time based on the frequency
         next_capture_time=$((next_capture_time + freq_seconds))
+        # Display the time of the next data capture
         echo -e "Next capture time: \e[93m$(date -d @$next_capture_time +%Y-%m-%d\ %H:%M:%S)\e[0m\033[A\033[K\033[A\033[K"
     fi
 done
